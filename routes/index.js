d5392e23d29a568f422d7c68103c4cb871b79bac
izekorphilip <izekorphilip5@gmail.com>
2024-10-17 05:42:55 +0100

    Week 3 pushed completely

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+// Base URL
+router.get('/', (req, res) => {
+    res.send("Contacts API")
+});
+
 router.use('/', require('./swagger'));
 router.use('/contacts', require('./contacts'));
 
